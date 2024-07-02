876de5d55f98c8e6ac342e589317282ae781f122
niarferuto <ing.efrain.orjuela@gmail.com>
2024-07-02 15:37:45 -0500

    alb name

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name    = "blog_alb"
+  name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
