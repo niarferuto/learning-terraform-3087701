28e4ddf4337c7090628497caf779800e0aaad7d3
niarferuto <ing.efrain.orjuela@gmail.com>
2024-07-01 22:59:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,6 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
-  subnet_id     = "subnet-09dac355a0c458a43"
 
   tags = {
     Name = "HelloWorld"
